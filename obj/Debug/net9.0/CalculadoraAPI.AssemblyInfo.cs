2c403eb4bb8af84c2e814944d0c4a1e00a92fe88
Yuri Sampaio <yurillsampaio@gmail.com>
2025-01-09 16:05:52 -0300

    Inicio do projeto

--- obj/Debug/net9.0/CalculadoraAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/CalculadoraAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f39fc210adb05320bce77dc072f094f7dd93d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40d66a0ff9de90e55e0f4a387a69a9d5674d3d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
